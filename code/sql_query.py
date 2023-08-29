c704662b468866697994606e1c54774d213c36e5
Riddis <sam.hoste@hotmail.com>
2023-08-29 16:33:33 +0200

    finished first visual

--- code/sql_query.py
+++ code/sql_query.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
+import streamlit as st
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib.ticker import ScalarFormatter, MaxNLocator
 
 # declarative base class
 Base = declarative_base()
@@ -36,7 +40,7 @@ question2query2 = text("""
 # Focus on countries with most sales
 question2query3 = text("""
              SELECT  
-             countries.name, sum(vintages.price_euros)          
+             countries.name, sum(vintages.price_euros) AS total_sold        
              FROM countries
              JOIN regions on regions.country_code = countries.code
              JOIN wines on wines.region_id = regions.id
@@ -50,7 +54,7 @@ question2query3 = text("""
 # Least sales = more room to grow
 question2query4 = text("""
              SELECT  
-             countries.name, sum(vintages.price_euros)          
+             countries.name, sum(vintages.price_euros) AS total_sold          
              FROM countries
              JOIN regions on regions.country_code = countries.code
              JOIN wines on wines.region_id = regions.id
@@ -69,7 +73,7 @@ question4query1 = text("""
             SELECT   
                 vintages.name,
                 wines.name,
-                group_concat(keywords_wine.group_name, ', ')
+                group_concat(distinct(keywords_wine.group_name))
             FROM 
                 wines
             JOIN keywords_wine on keywords_wine.wine_id = wines.id
@@ -80,44 +84,70 @@ question4query1 = text("""
             GROUP BY
                 vintages.name
             HAVING
-                COUNT(keywords.name) > 1
+                COUNT(keywords.name in ('coffee', 'toast', 'green apple', 'cream', 'citrus') AND keywords_wine.count > 10) > 1
             ORDER BY
                 wines.name;
              """
              )
 
-"""coffee
-toast
-green apple
-cream
-citrus"""
 # fetch data from db
-results = session.execute(question4query1)
+results1 = session.execute(question2query1)
+results2 = session.execute(question2query2)
+results3 = session.execute(question2query3)
+results4 = session.execute(question2query4)
+results5 = session.execute(question4query1)
 
-# print results
-for result in results:
-    print(result)
 
+df1 = pd.read_sql((question2query1), engine)
+df2 = pd.read_sql((question2query2), engine)
+df3 = pd.read_sql((question2query3), engine)
+df4 = pd.read_sql((question2query4), engine)
+df5 = pd.read_sql((question4query1), engine)
 
+#df1.index += 1
 
-""" JOIN toplists on countries.code = toplists.country_code
-             JOIN regions on countries.code = regions.country_code
-             JOIN wines on regions.id = wines.region_id
-             JOIN most_used_grapes_per_country on regions.country_code = most_used_grapes_per_country.country_code
-             JOIN toplists on toplists.country_code = countries.code
-             JOIN vintage_toplists_rankings on vintage_toplists_rankings.id = toplists.id
-query = text(
-             SELECT  
-             countries.name, countries.users_count, sum(vintages.price_euros)          
-             FROM vintage_toplists_rankings
-             JOIN vintages on vintages.id = vintage_toplists_rankings.vintage_id
-             JOIN toplists on toplists.id = vintage_toplists_rankings.top_list_id
-             JOIN wines on wines.id = vintages.wine_id
-             JOIN regions on regions.id = wines.region_id
-             JOIN countries on countries.code = regions.country_code
-             JOIN most_used_grapes_per_country on most_used_grapes_per_country.country_code = countries.code
-             JOIN grapes on grapes.id = most_used_grapes_per_country.grape_id
-             GROUP BY countries.name
-             order by countries.users_count desc
-             LIMIT 5;
-             """
+M = 5
+
+#fig = plt.figure(df1)
+plt.rcParams.update({'font.size': 20})
+
+fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(16, 12))
+
+ax1 = axes[0, 0]
+ax1.plot(df1.name, df1.users_count)
+ax1.set_title('Highest amount of users')
+ax1.set_xlabel('Name')
+ax1.set_ylabel('Users Count')
+ax1.yaxis.set_minor_formatter(ScalarFormatter())
+ax1.set_xticklabels(df1.name, rotation=45, ha="right")
+ax1.yaxis.set_major_locator(MaxNLocator(nbins=5))
+ax1.yaxis.set_minor_formatter(ScalarFormatter())
+ax1.ticklabel_format(style='plain', axis='y')
+
+ax2 = axes[0, 1]
+ax2.plot(df2.name, df2.users_count)
+ax2.set_title('Least amount of users')
+ax2.set_xlabel('Name')
+ax2.set_ylabel('Users Count')
+ax2.yaxis.set_major_formatter(ScalarFormatter(useOffset=False))
+ax2.set_xticklabels(df1.name, rotation=45, ha="right")
+
+ax3 = axes[1, 0]
+ax3.plot(df3.name, df3.total_sold)
+ax3.set_title('Most sales')
+ax3.set_xlabel('Name')
+ax3.set_ylabel('Total Sold')
+ax3.set_xticklabels(df1.name, rotation=45, ha="right")
+ax3.yaxis.set_major_locator(MaxNLocator(nbins=5))
+
+ax4 = axes[1, 1]
+ax4.plot(df4.name, df4.total_sold)
+ax4.set_title('Lowest sales')
+ax4.set_xlabel('Name')
+ax4.set_ylabel('Total Sold')
+ax4.set_xticklabels(df1.name, rotation=45, ha="right")
+ax4.yaxis.set_major_locator(MaxNLocator(nbins=5))
+
+fig.tight_layout()
+
+st.pyplot(fig.figure)
